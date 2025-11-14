4f117817d7fe56fd574c6b1f26c57ec9552e5601
Chencsi <kevinkeeeh@gmail.com>
2025-11-14 14:54:29 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Footer from './components/Footer'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <Home />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Made by Kevin Chen</p>
+    </footer>
+  )
+}
+
+export default Footer;
