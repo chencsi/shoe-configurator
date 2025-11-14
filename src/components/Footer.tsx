da3951763c0db973b65b8e39a55144afbed44d41
Chencsi <kevinkeeeh@gmail.com>
2025-11-14 15:23:48 +0100

    base footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <footer>
-      <p>Made by Kevin Chen</p>
-    </footer>
+    <div className="bg-white/50 backdrop-blur-xl text-center py-3 shadow">
+      <p className="text-zinc-600">Kevin Chen</p>
+    </div>
   )
 }
 
