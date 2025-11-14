78e85052a9b418e8fdae45473e7e203fb162d740
Chencsi <kevinkeeeh@gmail.com>
2025-11-14 18:01:18 +0100

    displaying current

--- src/components/ShoePreview.tsx
+++ src/components/ShoePreview.tsx
@@ -1,10 +1,16 @@
+import type { ShoeCustomization } from "../types/shoe";
 import Box from "./Box";
 
-function ShoePreview() {
+interface ShoePreviewProps {
+  customization: ShoeCustomization;
+}
+
+function ShoePreview({ customization }: ShoePreviewProps) {
   const title = "Előnézet";
 
   return (
     <Box title={title}>
+      <pre>{JSON.stringify(customization, null, 2)}</pre>
     </Box>
   )
 }
