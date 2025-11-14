94abf557c95a623ee4586d7894e4fe6bbd66f0c7
Chencsi <kevinkeeeh@gmail.com>
2025-11-14 16:05:51 +0100

    use components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,6 @@
-import Box from "./Box";
+import ColorSelector from "./ColorSelector";
+import OrderForm from "./OrderForm";
+import ShoePreview from "./ShoePreview";
 
 function Home() {
   return (
@@ -8,37 +10,10 @@ function Home() {
         <p className="text-lg text-zinc-600">Tervezd meg egyedi cipődet és rendeld meg most!</p>
       </div>
       <div className="grid grid-cols-2 gap-10">
-        <Box title="Előnézet">
-        </Box>
+        <ShoePreview />
         <div className="space-y-10">
-          <Box title="Testreszabás">
-            <div className="space-y-5">
-              <div className="space-y-2">
-                <p className="text-lg font-semibold">Felső színe</p>
-                <div className="grid grid-cols-2 gap-3">
-                  {["fehér", "fekete", "barna"].map(color => (
-                    <div key={color} className="flex flex-row items-center gap-3 border border-zinc-300 rounded-2xl p-3">
-                      <div className="h-15 w-15 border border-2 border-zinc-300 rounded-full" />
-                      <p className="text-xl">{color}</p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <div className="space-y-2">
-                <p className="text-lg font-semibold">Talp színe</p>
-                <div className="grid grid-cols-2 gap-3">
-                  {["fehér", "fekete", "barna"].map(color => (
-                    <div key={color} className="flex flex-row items-center gap-3 border border-zinc-300 rounded-2xl p-3">
-                      <div className="h-15 w-15 border border-2 border-zinc-300 rounded-full" />
-                      <p className="text-xl">{color}</p>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </Box>
-          <Box title="Rendelés">
-          </Box>
+          <ColorSelector />
+          <OrderForm />
         </div>
       </div>
     </div>
