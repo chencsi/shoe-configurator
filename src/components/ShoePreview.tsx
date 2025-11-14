953d3422a445187a2b48e250a7cc3f4ffbb5c7e6
Chencsi <kevinkeeeh@gmail.com>
2025-11-14 18:34:18 +0100

    displaying the shoe and customization works

--- src/components/ShoePreview.tsx
+++ src/components/ShoePreview.tsx
@@ -10,7 +10,41 @@ function ShoePreview({ customization }: ShoePreviewProps) {
 
   return (
     <Box title={title}>
-      <pre>{JSON.stringify(customization, null, 2)}</pre>
+      <div className="relative">
+        <img src="/sole.png" className="absolute inset-0" alt="sole" />
+        <img src="/top.png" className="relative" alt="top" />
+
+        <div
+          className="absolute inset-0"
+          style={{
+            backgroundColor: customization.top.color,
+            opacity: 0.5,
+            WebkitMaskImage: "url(/top.png)",
+            maskImage: "url(/top.png)",
+            WebkitMaskSize: "contain",
+            maskSize: "contain",
+            WebkitMaskRepeat: "no-repeat",
+            maskRepeat: "no-repeat",
+            WebkitMaskPosition: "center",
+            maskPosition: "center",
+          }}
+        />
+        <div
+          className="absolute inset-0"
+          style={{
+            backgroundColor: customization.sole.color,
+            opacity: 0.5,
+            WebkitMaskImage: "url(/sole.png)",
+            maskImage: "url(/sole.png)",
+            WebkitMaskSize: "contain",
+            maskSize: "contain",
+            WebkitMaskRepeat: "no-repeat",
+            maskRepeat: "no-repeat",
+            WebkitMaskPosition: "center",
+            maskPosition: "center",
+          }}
+        />
+      </div>
     </Box>
   )
 }
